5579a6bddfc73e84b81b8b909a8a32c33be0ce91
Frank Jobst <fajobst@gmail.com>
2021-05-27 19:11:52 -0500

    progress-new-to-preauth

--- src/main/java/guru/springframework/msscssm/services/PaymentService.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentService.java
@@ -2,16 +2,17 @@ package guru.springframework.msscssm.services;
 
 import guru.springframework.msscssm.domain.Payment;
 import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
 import org.springframework.statemachine.StateMachine;
 
 public interface PaymentService {
 
     Payment newPayment(Payment payment);
 
-    StateMachine<PaymentService, PaymentEvent> preAuth(Long paymentId);
+    StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
 
-    StateMachine<PaymentService, PaymentEvent> authorizePayment(Long paymentId);
+    StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
 
-    StateMachine<PaymentService, PaymentEvent> declineAuth(Long paymentId);
+    StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
 
 }
--- src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
+++ src/main/java/guru/springframework/msscssm/services/PaymentServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.statemachine.StateMachine;
 import org.springframework.statemachine.config.StateMachineFactory;
 import org.springframework.statemachine.support.DefaultStateMachineContext;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @AllArgsConstructor
 @Service
@@ -26,30 +28,33 @@ public class PaymentServiceImpl implements PaymentService {
         return paymentRepository.save(payment);
     }
 
+    @Transactional
     @Override
-    public StateMachine<PaymentService, PaymentEvent> preAuth(Long paymentId) {
-        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
-
-        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTHORIZE);
-
-        return null;
-    }
-
-    @Override
-    public StateMachine<PaymentService, PaymentEvent> authorizePayment(Long paymentId) {
+    public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 
         sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_APPROVED);
 
-        return null;
+        return sm;
     }
 
+    @Transactional
     @Override
-    public StateMachine<PaymentService, PaymentEvent> declineAuth(Long paymentId) {
+    public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+        StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
+
+        sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_APPROVED);
+
+        return sm;
+    }
+
+    @Transactional
+    @Override
+    public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
         StateMachine<PaymentState, PaymentEvent> sm = build(paymentId);
 
         sendEvent(paymentId, sm, PaymentEvent.PRE_AUTH_DECLINED);
-        return null;
+        return sm;
     }
 
     private void  sendEvent(Long paymentId, StateMachine<PaymentState, PaymentEvent> sm, PaymentEvent event) {
